ee17c70e4c2484b383866c72ded87faa7096888e
Brahim Naji <ba3105947@gmail.com>
2026-09-04 12:16:24 +0100

    Initial commit

---
+++ src/main/java/dev/brahim/cart/OrderPlatformApp.java
@@ -0,0 +1,13 @@
+package dev.brahim.cart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdaECommerceOrderPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdaECommerceOrderPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/brahim/cart/OrderPlatformAppTests.java
@@ -0,0 +1,13 @@
+package dev.brahim.cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdaECommerceOrderPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
